2f4bde7a51f61ad4e1eea8c5e00eacbadd9160af
Friedrich Horschig <frhorschig-coding@mailbox.org>
2025-07-31 17:53:26 +0200

    use base domain for letsencrypt path replacement

--- deployment/scripts/generate-config.sh
+++ deployment/scripts/generate-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <kant-search version number> <hostname>"
     exit 1
 fi
@@ -9,9 +9,8 @@ mkdir -p config/backend config/frontend
 curl -L -o config/backend/volume-metadata.json https://github.com/FrHorschig/kant-search-backend/releases/download/$1/volume-metadata.json
 curl -L -o config/frontend/config.json https://github.com/FrHorschig/kant-search-frontend/releases/download/$1/config.json
 
-sed -i -E \
-  -e "s|(\/etc/letsencrypt/live/)<hostname>|\1$2|g" \
-  -e "s|(https://)<hostname>|\1$2|g" \
-  kant-search-stack.yml
+base_domain=$(echo "$2" | awk -F. '{n=NF; print $(n-1)"."$n}')
+sed -i -E "s|(\/etc/letsencrypt/live/)<hostname>|\1$base_domain|g" kant-search-stack.yml
+sed -i -E "s|(https://)<hostname>|\1$2|g" kant-search-stack.yml
 sed -i -E "s|(domain = )<hostname>|\1$2|g" config/grafana/grafana.ini
 sed -i -E "s|(\"apiUrl\": \")http://localhost:5000|\1https://$2|g" config/frontend/config.json

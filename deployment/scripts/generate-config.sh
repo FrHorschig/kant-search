7ca914666add08a974a914c1bc231da11e1918f0
Friedrich Horschig <frhorschig-coding@mailbox.org>
2026-09-18 14:24:43 +0200

    use placeholder for container versions

--- deployment/scripts/generate-config.sh
+++ deployment/scripts/generate-config.sh
@@ -14,14 +14,14 @@ unzip ks-frontend-config.zip
 rm ks-frontend-config.zip
 cd ..
 
-# Replace hostname and base path placeholders
+# Replace placeholders
+sed -i "s|<version>|$1|g" kant-search-stack.yml
 sed -i "s|<hostname>|$2|g" kant-search-stack.yml
+sed -i "s|<base-path>|$3|" kant-search-stack.yml
+sed -i "s|<port>|$4|g" kant-search-stack.yml
+
 sed -i "s|<hostname>|$2|g" config/grafana/grafana.ini
 sed -i "s|http://localhost:5000|https://$2/${3:+$3/}|" config/frontend/config.json
-sed -i "s|<base-path>|$3|" kant-search-stack.yml
-
-# Replace port placeholders
-sed -i "s|<port>|$4|g" kant-search-stack.yml
 sed -i "s|<port>|$4|" config/reverse-proxy.conf
 
 # Create log directory

f951a501f9263986dbf4121596224f786db74ea4
Friedrich Horschig <frhorschig-coding@mailbox.org>
2026-09-18 08:48:48 +0200

    fix port handling and scheme forwarding

--- deployment/scripts/generate-config.sh
+++ deployment/scripts/generate-config.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <kant-search version number> <hostname> <base-path>"
-    echo "       base-path needs to include the "
+    echo "Usage: $0 <kant-search version number> <hostname> <base-path> <port>"
     exit 1
 fi
 
@@ -20,6 +19,10 @@ sed -i -E "s|(\/etc/letsencrypt/live/)<hostname>|\1$2|g" kant-search-stack.yml
 sed -i -E "s|(https://)<hostname>|\1$2" kant-search-stack.yml
 sed -i -E "s|(domain = )<hostname>|\1$2" config/grafana/grafana.ini
 sed -i -E "s|(\"apiUrl\": \")http://localhost:5000|\1https://$2/$3" config/frontend/config.json
+
+# Replace <port> placeholders
+sed -i -E "s|<port>|\1$4|g" kant-search-stack.yml
+sed -i -E "s|<port>|\1$4" config/reverse-proxy.conf
 
 # Create log directory
 mkdir -p log/backend
